5c52f7bcee0add3c50ae166410a2786c494745b4
Jing Li <jingli@rrc.ca>
2024-08-26 12:48:16 -0500

    firt commit on isd_demonstration

---
+++ course/course.py
@@ -0,0 +1,9 @@
+from department.department import Department
+
+class Course():
+    """
+    Represents template for a course
+    """
+    def __init__(self,name:str,department:Department ,credit_hours:int):
+        self.__name = name
+        
